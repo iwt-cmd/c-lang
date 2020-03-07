9906d102b63ebd75bf5219b7b8536588152bdbfc
Joe Banfield <jbanfield@hsm-hosting.com>
2020-03-07 13:47:32 -0500

    Firstcommit

---
+++ ptocalc/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/*
+    PTO Calculator
+
+    Functions:
+    - AccumTime2Date: amount of PTO to desired date
+    - AccumDate2Time: date needed to reach desired 
+*/
+int main(int argc, char *argv[]){
+    //constants
+    float jlb_perpay = 6.5;
+    int h2d = 8;
+    int d2w = 5;
+
+    //variables
+    float ptostart = atoi(argv[1]);
+    float ptodate = atoi(argv[2]);
+
+    printf("Arg 1 Function: %d\n", funct);
+    printf("Arg 2 Person : %d\n", person);
+    return 0;
+}
---
+++ test/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a[] = {12, 5, 3, 71, 109, 900};
+    int max;
+    int a_len = sizeof(a)/sizeof(int);
+
+    for (int i = 0; i < a_len; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+
+    printf("The max value is: %d\n", max);
+    return 0;
+}
