a215b7c812ed4f6b78c7ac9f00eb0fae78c06e64
Joe Banfield <jbanfield@hsm-hosting.com>
2020-03-07 16:46:39 -0500

    Reset "test.c" back to test template

--- test/test.c
+++ test/test.c
@@ -1,37 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
 /*
-    Test function for finding the max value in array a[]
+    Blank test template
 */
 int main(){
-    int a[10] = {0,0,0,0,0,0,0,0,0,0};
-    int max=0;
-    char t_val = 'n';
-    int count = 0;
-    while (t_val != 'd') {
-        printf("Enter a value (blank to finish): ");
-        scanf(" %s", &t_val);
-        if (!(t_val >= 'a' && t_val <='z')||!(t_val >= 'A' && t_val <= 'Z')) {
-            a[count] = atoi(&t_val);
-            //printf("A value: %d\n", a[count]);
-            count++;
-            if (count == 10) {
-                break;
-            }
-        } else if (t_val == 'd' || t_val == EOF){
-            break;
-        }
-        else {
-            printf("Not a valid entry");
-        }
-    } 
-    for (int i = 0; i <= count-1; i++) {
-        if (a[i] > max) {
-            max = a[i];
-        }
-    }
-    printf("Number of values: %d\n", count-1);
-    printf("The max value is: %d\n", max);
     return 0;
 }
